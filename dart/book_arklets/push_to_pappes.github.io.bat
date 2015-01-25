d20853c58cb73308d386532bc0e390908ccf4dcb
pappes <pappes@gmail.com>
2015-01-25 20:34:26 +1000

    better logging

--- dart/book_arklets/push_to_pappes.github.io.bat
+++ dart/book_arklets/push_to_pappes.github.io.bat
@@ -1,4 +1,4 @@
-xcopy /y /s build ..\..\..\pappes.github.io\book_arklets
+xcopy /y /s build ..\..\..\pappes.github.io\book_arklets\build
 cd ..\..\..\pappes.github.io\book_arklets\build\packages
 call "duplicate libraries.bat"
 pause
--- dart/book_arklets/web/remove_html_overlays.dart
+++ dart/book_arklets/web/remove_html_overlays.dart
@@ -1,6 +1,15 @@
 import 'dart:html';
+import 'package:logging/logging.dart' as logging;
 import 'package:pappes_web_utility/pappes_web_utility.dart';
 
 void main() {
+
+  logging.hierarchicalLoggingEnabled = true;
+  log.level = logging.Level.ALL;
+  log.onRecord.listen((logging.LogRecord rec) {
+    print('${rec.level.name}: ${rec.time}: ${rec.message}');
+  });
+  
+  
   MyHtml.removeAllOverlays(document);  
 }
