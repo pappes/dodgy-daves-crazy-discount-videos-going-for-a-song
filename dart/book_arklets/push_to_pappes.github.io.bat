e5eb9485c368abc3a2a96434f3321a0e21eb1a64
pappes <pappes@gmail.com>
2016-06-30 21:40:44 +1000

    include dart artefacts in external copy of bookmarklets

--- dart/book_arklets/push_to_pappes.github.io.bat
+++ dart/book_arklets/push_to_pappes.github.io.bat
@@ -1,3 +1,4 @@
 xcopy /y /s build ..\..\..\pappes.github.io\book_arklets\build
+xcopy /y /s web ..\..\..\pappes.github.io\book_arklets\build\web
 cd ..\..\..\pappes.github.io\book_arklets\build\packages
 call "duplicate libraries.bat"
