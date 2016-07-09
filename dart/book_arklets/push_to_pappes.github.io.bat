09a59f616c3a966cf056c2fbda6987ce96f1b9c8
pappes <pappes@gmail.com>
2016-07-09 20:37:22 +1000

    move logging into common code

--- dart/book_arklets/push_to_pappes.github.io.bat
+++ dart/book_arklets/push_to_pappes.github.io.bat
@@ -1,4 +1,11 @@
-xcopy /y /s build ..\..\..\pappes.github.io\book_arklets\build
-xcopy /y /s web ..\..\..\pappes.github.io\book_arklets\build\web
+xcopy /f /y /s /c build ..\..\..\pappes.github.io\book_arklets\build
+xcopy /f /y /s /c /d /e /v /h web ..\..\..\pappes.github.io\book_arklets\build\web
+xcopy /f /y /s /c web\packages ..\..\..\pappes.github.io\book_arklets\build\web\packages
+copy /y web\packages\browser ..\..\..\pappes.github.io\book_arklets\build\web\packages\browser
+copy /y web\packages\logging ..\..\..\pappes.github.io\book_arklets\build\web\packages\logging
+copy /y web\packages\pappes_utility ..\..\..\pappes.github.io\book_arklets\build\web\packages\pappes_utility
+copy /y web\packages\pappes_utility\src ..\..\..\pappes.github.io\book_arklets\build\web\packages\pappes_utility\src
+copy /y web\packages\pappes_web_utility ..\..\..\pappes.github.io\book_arklets\build\web\packages\pappes_web_utility
+copy /y web\packages\pappes_web_utility\src ..\..\..\pappes.github.io\book_arklets\build\web\packages\pappes_web_utility\src
 cd ..\..\..\pappes.github.io\book_arklets\build\packages
 call "duplicate libraries.bat"
