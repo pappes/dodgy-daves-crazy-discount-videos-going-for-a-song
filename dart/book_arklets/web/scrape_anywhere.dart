aca70f709d26e40a2b19c29e33e335b018ed5db2
pappes <pappes@gmail.com>
2022-04-16 00:16:26 +1000

    update anywhere scraper

--- dart/book_arklets/web/scrape_anywhere.dart
+++ dart/book_arklets/web/scrape_anywhere.dart
@@ -7,7 +7,11 @@ void main() {
   //use common function to set up logging
   turnOnLogging();
   //change all href Elements in the body to target the current tab
-  final map = MyWebScraper.scrapeAnywhereList();  
+  String result = 'events=\n';
+  final events = MyWebScraper.scrapeAnywhereList();  
+  for (final event in events){
+    result += '${mapToCSV(event)}/n';
+  }
 
-    window.navigator.clipboard?.writeText(mapToCSV(map));
+    window.navigator.clipboard?.writeText(result);
 }
